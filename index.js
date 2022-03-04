313a04b6f1bc7e0ea22b62ebd4b3703100620579
Juan Toro <jct20xx@gmail.com>
2022-03-04 14:48:36 -0500

    commit tree 41823f748086769aad83be1e829128e47361562d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+    return cats.push(cat);
+};
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat);
+};
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+};
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+};
+function appendCat(name) {
+    const newCats = [...cats,name]
+    return newCats;
+};
+function prependCat(name) {
+    const newCat = [name, ...cats]
+    return newCat;
+};
+function removeLastCat() {
+    const removeCat = cats.slice(0,-1)
+    return removeCat;
+};
+function removeFirstCat() {
+    const removeCats = cats.slice(1)
+    return removeCats;
+};
